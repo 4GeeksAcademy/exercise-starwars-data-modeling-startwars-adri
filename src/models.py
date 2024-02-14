dfacd8dac7884e18b8b72b8074ef699bb2e83c0c
AdrianRios25 <144917403+AdrianRios2527@users.noreply.github.com>
2024-02-14 17:52:47 +0000

    read

--- src/models.py
+++ src/models.py
@@ -8,14 +8,14 @@ from eralchemy2 import render_er
 Base = declarative_base()
 
 class Person(Base):
-    __tablename__ = 'person'
+    _tablename_ = 'person'
     # Here we define columns for the table person
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
 
 class Address(Base):
-    __tablename__ = 'address'
+    _tablename_ = 'address'
     # Here we define columns for the table address.
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
@@ -24,6 +24,31 @@ class Address(Base):
     post_code = Column(String(250), nullable=False)
     person_id = Column(Integer, ForeignKey('person.id'))
     person = relationship(Person)
+
+class FavoriteItem(Base):
+    _tablename_ = 'favorite_items'
+    user_id = Column(Integer, ForeignKey('user.id'), primary_key=True, nullable=False)
+    planet_id = Column(Integer, ForeignKey('planets.id'), nullable=True)
+    character_id = Column(Integer, ForeignKey('characters.id'), nullable=True)
+class User(Base):
+    _tablename_ = 'user'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    favorites = relationship('FavoriteItem', back_populates='user')
+class Planets(Base):
+    _tablename_ = 'planets'
+    id = Column(Integer, primary_key=True)
+    diameter = Column(String(250), nullable=True)
+    name = Column(String(250), nullable=True)
+    population = Column(Integer)
+    favorite_id = relationship('FavoriteItem', backref='planets')
+class Characters(Base):
+    _tablename_ = 'characters'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250))
+    last_name = Column(String(250))
+    height = Column(Float)
+    favorite_id = relationship('FavoriteItem', backref='characters')
 
     def to_dict(self):
         return {}
